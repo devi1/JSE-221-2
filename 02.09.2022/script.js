1e72d416f1d09767261f34685aeecf980b767baa
Damir <damiryessenbek@gmail.com>
2022-09-02 21:48:10 +0600

    Работа со встроенными объектами в JavaScript

---
+++ 02.09.2022/script.js
@@ -0,0 +1,111 @@
+// let myArray = new Array();
+// myArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
+
+// alert(myArray.indexOf(4));
+
+// let myArray2 = myArray.slice();
+
+// alert(myArray2);
+// myArray2 = myArray.splice(2, 4);
+// alert(myArray2);
+
+// Math.E
+
+// Math.LN2
+// Math.LN10
+
+// Math.PI
+
+// Math.SQRT1_2
+// Math.SQRT2
+
+// Math.abs();
+// Math.sqrt();
+// Math.floor();
+// Math.ceil();
+// Math.round();
+
+// Math.pow(x, y);
+
+// Math.random();
+
+// Math.trunc();
+
+// let now = new Date();
+// alert(now);
+
+// let jan2 = new Date(24*3600*1000);
+// alert(jan2);
+
+// let date = new Date();
+
+// alert(date.getFullYear());
+
+// alert(date.getMonth()+1);
+
+// alert(date.getDay());
+
+// alert(date.getDate());
+
+// alert(date.getHours());
+
+// alert(date.getUTCHours());
+
+// alert(date.getTimezoneOffset());
+
+// let today = new Date();
+
+// today.setFullYear(2017, 3, 0);
+// setMonth(month[0-11], Date[1-31]);
+// setDate(Datep[1-31]);
+// today.setHours()
+// today.setMinutes()
+// today.setSeconds()
+// today.setMilliseconds()
+// alert(today);
+
+// let str = prompt("Please enter a random sentence or word");
+
+// function change(str){
+    //     let strChange = "";
+    //     let numStr = str.split("");
+
+    //     for (let i = 0; i < str.length; i++) {
+        //         if (str[i] == str[i].toUpperCase()) {
+            //             strChange += str[i].toLowerCase();
+            //         }
+//         else if (str[i] == str[i].toLowerCase()){
+//             strChange += str[i].toUpperCase();
+//         }
+//         else if(Number(numStr[i]) === typeof 1){
+    //             strChange += "_";
+//         }
+
+//         alert(typeof numStr[i]);
+//     }
+
+//     return strChange;
+// }
+// alert(change(str));
+// let str = "sadAdfd55";
+
+// function change(str){
+//     let newStr = str.split("");
+
+//     for(let i = 0; i < newStr.length; i++) {
+//         if (newStr[i] == newStr[i].toUpperCase()){
+//             newStr[i] = newStr[i].toLowerCase();
+//         }
+//         else if (newStr[i] == newStr[i].toLowerCase()){
+//             newStr[i] = newStr[i].toUpperCase();
+//         }
+//         else if (newStr[i].match(/\d+/)){
+//             newStr[i] = "_";
+//         }
+//     }
+
+//     return newStr;
+// }
+
+// alert(change(str));
+
