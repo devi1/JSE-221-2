9a110cb2f604d2283ec6c90f904d386e7d3b9073
Damir <damiryessenbek@gmail.com>
2022-09-16 20:17:37 +0600

    регулярные выражения

--- 16.09.2022/script.js
+++ 16.09.2022/script.js
@@ -1,41 +1,22 @@
-// let str = prompt("What do you want to find?")
-
-// regexp = new RegExp(`${str}`);
-
 let str = `1 tasdasd 9
 2 from asdasd 8
 3 to asdasd 99`
 
+let newStr = [];
 
-// alert(str)
-// alert(str.match(/./gim)); 
 
-// let newStr = [];
+let arrEn = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '\n'];
 
-// let newStrDigits = str.match(/\d/gm);
-// let newStrAll = str.match(/./gm)
-// alert(newStrAll);
-// alert(newStrDigits);
-
-alert(str.length)
-
-let arrEn = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-alert(arrEn.length);
-function getMax(numberOne, numberTwo){
-    if(numberOne > numberTwo) return numberOne;
-    else return numberTwo;
+for(let i = 0; i < str.length; i++){
+    for(let j = 0; j < arrEn.length; j++){
+        if(str[i] == arrEn[j]){
+            newStr.push(str[i]);
+        }
+    }
 }
 
-alert(getMax(str.length, arrEn.length));
+alert(newStr);
 
-// for(let i = 0; i < getMax(str.length, arrEn.length); i++) {
+newStr = newStr.join("");
 
-// }
-
-// for(let i = 0; i < str.length; i++){
-//     if(str.match(/./gm)[i] != str.match(/\d/gm)){
-//         newStr.push(str[i]);
-//     }
-// }
-
-// alert(newStr);
+alert(newStr);
